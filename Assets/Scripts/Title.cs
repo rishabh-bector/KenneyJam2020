16a7a9734c474d5269bf3594995fb1226f8fed56
Rishabh Bector <bector.rishabh@gmail.com>
2020-08-22 13:59:34 -0500

    more UI work

---
+++ Assets/Scripts/Title.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour {
+
+    public void StartGame() {
+        SceneManager.LoadScene("gameScene");
+    }
+
+    public void ExitGame() {
+        Application.Quit();
+    }
+}
